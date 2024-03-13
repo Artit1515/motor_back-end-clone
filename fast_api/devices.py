9664aeb9c1ef33ed4119cce755ca5ac097f5513f
Bright_Peerapat <Peerapat@example.com>
2024-03-14 00:29:56 +0700

    update hash

--- fast_api/devices.py
+++ fast_api/devices.py
@@ -22,12 +22,14 @@ class Sensors(BaseModel): # Model for only sensors that will be call from web-ap
 
 class Motor_data(BaseModel):
     motor_id: str
-    sensors: dict
+    sensors: list = []
 
 class Motor_info(BaseModel):
     motor_id: str
+    motor_name: str
     location: str
     series: str
+    department: str
     create_on: datetime = datetime.now()
 
 class Motor_id(BaseModel):
@@ -57,33 +59,42 @@ async def find_motor(mt_data: Motor_id):
         raise HTTPException(status_code=404)
 
 @router.delete("/motor/delete")
-async def delete_motor(motor_id):
-    motor = motor_info_coll.find_one(dict(motor_id))
-    if motor :
-        motor_info_coll.delete_one(dict(motor_id))
+async def delete_motor(motor_id: Motor_id):
+    try:
+        motor = motor_info_coll.find_one(motor_id.model_dump())
+        if motor :
+            motor_info_coll.delete_one(motor_id.model_dump())
+    except Exception as e:
+        raise HTTPException(status_code=404, detail=str(e))
 
-#___ Motor Data / Sensor ___
+###___ Motor Data / Sensor ___###
+
+# Adding sensors data from esp
 @router.post("/sensor/store")
 async def store_data(mt_data: IoT_Recv):
+    sensor = {
+        "temperature": mt_data.temperature,
+        "vibration": mt_data.vibration,
+        "voltage": mt_data.voltage,
+        "current": mt_data.current,
+        "rpm": mt_data.rpm,
+        "timestamp": datetime.now()
+    }
     motor_exist = motor_data_coll.find_one({"motor_id":mt_data.motor_id})
     if motor_exist:
-        sensor = {
-            "temperature": mt_data.temperature,
-            "vibration": mt_data.vibration,
-            "voltage": mt_data.voltage,
-            "current": mt_data.current,
-            "rpm": mt_data.rpm,
-            "timestamp": datetime.now()
-        }
         ft = {"motor_id": mt_data.motor_id}
         update = {"$push": {"sensors": sensor}}
-        motor_data_coll.update_one(ft,update=update, upsert=True)
-        # add_data(mt_data)
+        motor_data_coll.update_one(ft,update=update, upsert=True) #push new data to sensors array
         return { "msg": "Added new data." }
     else:
-        motor_data_coll.insert_one(mt_data.model_dump())
+        motor = {
+            "motor_id": mt_data.motor_id,
+            "sensors": [sensor] #create array with first sensors data
+        }
+        motor_data_coll.insert_one(motor)
         return { "msg": "Stored data to database." }
 
+# Getting sensors data from web-app
 @router.post("/get/motor_data")
 async def get_motor_data(motor_id: Motor_id):
     motor = motor_data_coll.find_one(motor_id.model_dump())
--- fast_api/users.py
+++ fast_api/users.py
@@ -1,16 +1,19 @@
 import uuid
+import hashlib
 from fastapi import APIRouter, HTTPException
 from pydantic import BaseModel
 from datetime import datetime
 from database import db
+from devices import motor_data_coll, motor_info_coll
 
 class Register(BaseModel):
-    user_id: str = str(uuid.uuid4())
+    user_id: str = ""
     username: str
     email: str
     passwd: str
-    motor_owned: list = []
+    role: str = "customer"
     create_on: datetime = datetime.now()
+    motor_owned: list = []
     
 class Login(BaseModel):
     email: str
@@ -25,9 +28,20 @@ class AddMotor(BaseModel):
     user_id: str
     motor_id: str
     motor_name: str
+
+class User_id(BaseModel):
+    user_id: str
     
 router = APIRouter()
 user_coll = db.collection("users")
+
+# Function to hash the password
+def hash_password(password: str) -> str:
+    algorithm = "sha256"
+    hash_obj = hashlib.new(algorithm)
+    hash_obj.update(password.encode("utf-8"))
+    hashed_password = hash_obj.hexdigest()
+    return hashed_password
 
 @router.get("/")
 async def hello_user():
@@ -35,23 +49,37 @@ async def hello_user():
 
 @router.post("/register")
 async def register(user: Register):
+    user_exist = user_coll.find_one({"email": user.email})
+    if user_exist:
+        raise HTTPException(status_code=400, detail="Email already used.")
+    #hash the password
+    hashed_passwd = hash_password(user.passwd)
+    user.passwd = hashed_passwd
+    #assign user_id
+    user_id = str(uuid.uuid4())
+    user.user_id = user_id
     user_coll.insert_one(user.model_dump())
-    return {"msg": "User added"}
+    return {"msg": "Registering successful."}
 
 @router.post("/login")
 async def login(user: Login):
-    usr = user_coll.find_one(user.model_dump())
-    if usr :
-        return { "msg": "Logged in successfully." }
-    else:
-        raise HTTPException(status_code=404)
+    usr = user_coll.find_one({"email": user.email})
+    if usr is None:
+        raise HTTPException(status_code=404, detail="User not found.")
     
+    hased_passwd = hash_password(user.passwd)
+    if hased_passwd != usr["passwd"]:
+        raise HTTPException(status_code=401, detail="Incorrect password.")
+    return { "msg": "Logged in successfully." }
+
+# Adding motor by customer
 @router.post("/add/motor")
 async def add_motor_owned(info: AddMotor):
     try:
         user = user_coll.find_one({"user_id": info.user_id})
-        if not user:
-            raise HTTPException(status_code=404, detail="User not found")
+        motor = motor_info_coll.find_one({"motor_id": info.motor_id})
+        if (not user) or (not motor):
+            raise HTTPException(status_code=404, detail="User or Motor not found")
         
         motor_info = {
             "motor_id": info.motor_id,
@@ -64,3 +92,20 @@ async def add_motor_owned(info: AddMotor):
         return {"msg": "Motor added successfully", "motor_id": info.motor_id}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
+
+
+@router.post("/get/user_data")
+async def get_user_data(user_id: User_id):
+    try:
+        user = user_coll.find_one(user_id.model_dump())
+        if not user:
+            raise HTTPException(status_code=404, detail="User not found")
+        
+        return {
+            "user_id": user["user_id"],
+            "username": user["username"],
+            "role": user["role"],
+            "motor_owned": user["motor_owned"]
+        }
+    except Exception as e:
+        raise HTTPException(status_code=404, detail=str(e))
